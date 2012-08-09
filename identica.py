3b83db8b84f3a2785f23f314c1b38f2153e8a16e
David Haberthür <email@davidhaberthuer.ch>
2012-08-09 17:12:45 +0200

    update
    This is the version of the file used for
    http://status.davidhaberthuer.ch/notice/25576

--- identica.py
+++ identica.py
@@ -1,11 +1,12 @@
 # Loads notices from identi.ca via the API, saves the dates of the notices and plots them
 
 import urllib2
-from xml.dom.minidom import parseString # from http://is.gd/IQUOew
+# The XML-wrangling code is based on http://is.gd/IQUOew
+from xml.dom.minidom import parseString 
 import datetime
 from pylab import *
 
-howmany = 50
+howmany = 250
 
 # Load freshest notice ID
 try:
@@ -31,15 +32,14 @@ for Notice in range(1,ID,int(ID/howmany)):
 		file.close()
 		dom = parseString(data)
 		Tag = dom.getElementsByTagName('created_at')[0].toxml()
-		#strip off the tag (<tag>data</tag>  --->   data):
 		Date=str(Tag.replace('<created_at>','').replace('</created_at>',''))
-		Date = str(Date[:-11])+str(Date[-5:])
+		Date = str(Date[:-11])+str(Date[-5:]) # strip the UTC shift, since I just couldn't get the %z parameter to work...
 		Date = datetime.datetime.strptime(Date,'%a %b %d %H:%M:%S %Y')
 		print '[' + str(counter) + '/' + str(howmany) + '] ID',Notice,'was posted on',Date
 		plt.plot_date(Date,Notice)
 		plt.title('Identica Notice Number vs. Date')
 	except:
-		# Try the next one if this one doesnt exist
+		# Try the next notice if this one doesnt exist
 		try:
 			Notice += 1
 			file = urllib2.urlopen('http://identi.ca/api/statuses/show/'+str(Notice)+'.xml')
@@ -47,7 +47,6 @@ for Notice in range(1,ID,int(ID/howmany)):
 			file.close()
 			dom = parseString(data)
 			Tag = dom.getElementsByTagName('created_at')[0].toxml()
-			#strip off the tag (<tag>data</tag>  --->   data):
 			Date=str(Tag.replace('<created_at>','').replace('</created_at>',''))
 			Date = str(Date[:-11])+str(Date[-5:])
 			Date = datetime.datetime.strptime(Date,'%a %b %d %H:%M:%S %Y')
