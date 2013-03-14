1ab7002055c886a1d57b182b1ed0e781d7ffd7fd
David Haberthür <email@davidhaberthuer.ch>
2013-03-14 15:02:57 +0100

    Formatting changes for crossfire.py
    Mostly to make it `pep8` compliant

--- crossfire.py
+++ crossfire.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 
 """
-*Cr*appy *O*pen *S*ource *S*oftware: *F*eedreader *I*n *R*esponse to *E*van
+				  *Cr*appy *O*pen *S*ource *S*oftware:
+				 *F*eedreader *I*n *R*esponse to *E*van
 
 This aims to be the 'crappiest Open Source RSS client' to have less
 'features than Google Reader' on July 2nd 2013. It's supposed to be
 funny: http://goo.gl/b9zdm
 
 Copyright 2013 David Haberthür
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
 
-		http://www.apache.org/licenses/LICENSE-2.0
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 from optparse import OptionParser
@@ -32,19 +34,20 @@ import time
 parser = OptionParser()
 usage = "usage: %prog [options] arg"
 parser.add_option('-f', '--Feed', dest='Feed',
-                  help='URL of the feed you want to read',
-                  metavar='URL')
+				  help='URL of the feed you want to read',
+				  metavar='URL')
 (options, args) = parser.parse_args()
 
 # show the help if no parameters are given
 if options.Feed is None:
-    parser.print_help()
-    print 'Example:'
-    print 'The command below reads the Atom feed of http://identi.ca'
-    print ''
-    print sys.argv[0], '-f http://identi.ca/api/statuses/public_timeline.atom'
-    print ''
-    sys.exit(1)
+	parser.print_help()
+	print 'Example:'
+	print 'The command below reads the Atom feed of http://identi.ca'
+	print ''
+	print (sys.argv[0],
+		   '-f http://identi.ca/api/statuses/public_timeline.atom')
+	print
+	sys.exit(1)
 
 starttime = time.time()
 print 'The current time is',time.strftime("%a, %d %b %Y %H:%M:%S +0000",
@@ -63,5 +66,6 @@ print '    * at the URL "' + f.entries[1].link + '"'
 print
 print 'The current time is',time.strftime("%a, %d %b %Y %H:%M:%S +0000",
 										  time.localtime())
-print 'It took me', time.time() - starttime, 'seconds to do your bidding.'
+print ('It took me ' + str(round(time.time() - starttime,3)) + 
+	   ' seconds to do your bidding.')
 print 
