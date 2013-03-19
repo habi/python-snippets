77dc8ddbf571fbd914b41f9f2b8011ac499fc1cc
David Haberthür <email@davidhaberthuer.ch>
2013-03-19 22:51:14 +0100

    blendit.py
    This is the version for the blog-post: http://wp.me/p4u1r-Nv

--- blendit.py
+++ blendit.py
@@ -22,16 +22,17 @@ for Image in range(1,13):
 		pass
 	else:
 		try:
-			os.mkdir(os.path.join(BasePath,"%02d" % Image + '_' + "%02d" % (Image + 1)))
+			os.mkdir(os.path.join(BasePath,"%02d" % Image + '_' + "%02d" % (Image %12 + 1)))
 		except OSError:
 			pass
+	print
 	# Go through every blending level from 1 to 100%, don't use 0, since that is
 	# 100% from the previous call.
-	for blendinglevel in range(1,101,10):
-		# Images go from 1 to 12 for each month. We use 'Image' and 'Image + 1'
-		# in each loop to construct the call for ImageMagick. Since we want to
-		# merge December with January, we can simply use the Image-number 
-		# modulo 12 (Image % 12), which gives 1 for 13.
+	for blendinglevel in range(1,101):
+		# 'Image' goes from 1 to 12 (each month). We use 'Image' and 'Image + 1'
+		# in each loop to construct the call for ImageMagick. Since we also want
+		# to merge December with January, we can simply use the Image-number 
+		# modulo 12 (Image % 12), which gives 1 for 13. Voila!
 		ImageName = "%02d" % Image + '_' + "%02d" % (Image % 12 + 1) + '_' +\
 			"%03d" % blendinglevel + '.jpg'
 		print str(blendinglevel) +  '% of', '%02d' % Image + '_stereographic.tif +',\
@@ -48,10 +49,11 @@ for Image in range(1,13):
 		blendcommand += '-distort SRT ' + str(360.0/12*blendinglevel/100+((Image-1)*360/12))
 		# blending requires alpha channel
 		blendcommand += ' -alpha on '
-		# since the rotation leaves a background, we just shave 1000 px off and
-		# get rid of that background. There probably is an option to 'rotate',
-		# but this is the KISS version.
-		blendcommand += '-shave 1000x1000'
+		# since the rotation leaves a background, we just shave off the edge of
+		# the images and crop them to a 4:3 aspect ration to get rid of the
+		# background. There probably is an option to 'rotate', but this is the
+		# KISS version.
+		blendcommand += '-shave 1000x1250'
 		# blend the two images together with a varying blending level
 		blendcommand += ' -compose blend -define compose:args=' + "%03d" % blendinglevel + ' -composite '
 		# to a desired output filename (in a subfolder, constructed through
@@ -62,5 +64,6 @@ for Image in range(1,13):
 		else:
 			os.chdir(BasePath)
 			os.system(blendcommand)
+	print 'Done with month',Image,'and',Image % 12 + 1
 
 print 'been there, done that!'
