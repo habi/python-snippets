ec60f493a261c7d90062ea7aae9060143fb7844c
David Haberthür <email@davidhaberthuer.ch>
2013-05-08 10:53:00 +0200

    Timealpse script updated and pep8yfied
    - code cleanup
    - to save some space we delete the hidden .svn and the movie directory,
    since we don't need them for the timelapse of the paper. Deleting with
    shutils.rmtree, according to http://stackoverflow.com/a/303225

--- acinuspaper-timelapse.py
+++ acinuspaper-timelapse.py
@@ -5,24 +5,31 @@ I can then generate some kind of "timelapse" of the evolution of the paper.
 
 import os
 import subprocess
+import shutil
 
 # Setup
-SaveToDirectory = os.path.join('c:','Users','haberthu','Desktop','Dropbox','Work','AcinusPaperTimeLapse')
+SaveToDirectory = os.path.join('c:', 'Users', 'haberthu', 'Desktop',
+    'Dropbox', 'Work', 'AcinusPaperTimeLapse')
 
 # Get SVN info from remote repository
-(Output, Error) = subprocess.Popen('svn info http://code.ana.unibe.ch/svn/AcinusPaper',
-    stdout=subprocess.PIPE, shell=True).communicate()
+(Output, Error) = subprocess.Popen(('svn', \
+                                    'info',\
+                                    'http://code.ana.unibe.ch/svn/' + \
+                                    'AcinusPaper'),
+                                    stdout=subprocess.PIPE,
+                                    shell=True).communicate()
 
 # Do some string manipulation to find the highest revision number
 # The 'Output' above contains 'Revision: $RevisionNumber', thus find the first
-# occurrence of 'Revision' in Output, and use the two digits that come after it.
+# occurrence of 'Revision' in Output, and use the two digits that come after it
 # If we have more than 99 revisions 2 will need to be changed to 3. :)
-MaxRevision = int(Output[Output.find('Revision')+len('Revision: '):
-                         Output.find('Revision')+len('Revision: ')+2])
+MaxRevision = int(Output[Output.find('Revision') + len('Revision: '):
+                         Output.find('Revision') + len('Revision: ') + 2])
 
-# Check out each revision into its own directory
-for Revision in range(1,MaxRevision+1):  # go from 1 to MaxRevision, not between
-    SavePath = os.path.join(SaveToDirectory,'PaperRevision' + "%02d" % Revision)
+# Check out each revision into its own directory, remove unnecessary files
+for Revision in range(1, MaxRevision + 1):  # go from 1 to MaxRev, not between
+    SavePath = os.path.join(SaveToDirectory,
+                            'PaperRevision' + "%02d" % Revision)
     print 'Checking out revision', Revision, 'of the acinus manuscript to',\
         SavePath
     # See if we can make a directory. If not, it might already exist. If not,
@@ -34,13 +41,20 @@ for Revision in range(1,MaxRevision+1):  # go from 1 to MaxRevision, not between
             print 'I do not know what is wrong'
             exit()
     # Redirect subprocess output to Nirvana: http://stackoverflow.com/a/1244757
-    nirvana = open("NUL","w")
-    subprocess.call('svn checkout -r ' + str(Revision) + 
-        ' http://code.ana.unibe.ch/svn/AcinusPaper ' + SavePath, 
-        stdout=nirvana, stderr = nirvana, shell=True)
+    nirvana = open("NUL", "w")
+    subprocess.call('svn checkout -r ' + str(Revision) +
+        ' http://code.ana.unibe.ch/svn/AcinusPaper ' + SavePath,
+        stdout=nirvana, stderr=nirvana, shell=True)
     #~ subprocess.Popen('latexmk ' + str(os.path.join(SavePath, 'acinus.tex')),
         #~ stdout=nowhere, stderr = nowhere)
     # Nirvana was probably a bad choice of variable name, since the Nirvana
     # cannot be closed
     nirvana.close()
-    os.rename(os.path.join(SavePath, 'acinus.tex'),os.path.join(SavePath, 'acinus_rev' + "%02d" % Revision + '.tex'))    
+    os.rename(os.path.join(SavePath, 'acinus.tex'),
+              os.path.join(SavePath, 'acinus_rev' + "%02d" % Revision +
+                           '.tex'))
+    # remove SVN directory, we don't need that
+    shutil.rmtree(os.path.join(SavePath, '.svn'))
+    # remove movies for larger revisions, we don't need them
+    if Revision > 57:
+        shutil.rmtree(os.path.join(SavePath, 'movies'))
