a961765a25d93871aca831eb411f3b500602f980
David Haberthür <email@davidhaberthuer.ch>
2013-10-21 18:05:01 +0200

    Trying to scrape some dopplr content
    Dopplr is going away, this might help the archive-team (or not).
    Just simply brute forcing hashes, since that's what they seem to do.
    The 'currenthash' on line 12 is one I got in 2009...

---
+++ hasher.py
@@ -0,0 +1,38 @@
+"""
+generate some random numbers, get a pdf from this random link...
+"""
+
+import os
+import hashlib
+import urllib
+import urllib2
+import sys
+
+# Trying to download some personal reports
+currenthash = "7fb16e238d5659a6"
+base_url = "http://reports.dopplr.com/2009/"
+try:
+    os.mkdir(os.path.join(os.getcwd(), 'dopplr'))
+except:
+    'Folder "dopplr" already exist, proceeding...'
+HowMany = int(1e6)
+print 'Trying to get', HowMany, 'personal reports from', base_url
+for i in range(HowMany):
+    random_data = os.urandom(128)
+    currenthash = hashlib.md5(random_data).hexdigest()[:16]
+    print str(i) + '/' + str(HowMany) + ":", currenthash + '.pdf',
+    if os.path.isfile(os.path.join(os.getcwd(), 'dopplr',
+                      currenthash + "_na.txt")):
+        print 'which we already tried before'
+    else:
+        filename = str(base_url + currenthash + ".pdf")
+        try:
+            urllib2.urlopen(filename)
+            urllib.urlretrieve(filename, os.path.join(os.getcwd(), 'dopplr',
+                               currenthash + ".pdf"))
+            print 'saved as', os.path.join('dopplr', currenthash + ".pdf")
+        except:
+            print 'is not there'
+            open(os.path.join(os.getcwd(), 'dopplr', currenthash + "_na.txt"),
+                 'w').close()
+            sys.stdout.write("\033[F") # Cursor up one line
